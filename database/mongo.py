60599dfa46f479b3e3db54c1442510f8a0cd5a6b
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-08-14 16:23:57 -0400

    only db

--- database/mongo.py
+++ database/mongo.py
@@ -1,15 +1,7 @@
 import os
 import motor.motor_asyncio
-from models.users import UserInDB
 
 async def get_db():
     mongo_uri = os.getenv('MONGO_URI')
     client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri)
     return client.otakusenpai
-
-
-#Fake
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
