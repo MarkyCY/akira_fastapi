e4ef792fbe3a8f8eab90a9956093642233db225f
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-08-14 16:48:18 -0400

    Add load env

--- database/mongo.py
+++ database/mongo.py
@@ -1,6 +1,9 @@
 import os
 import motor.motor_asyncio
 
+from dotenv import load_dotenv
+load_dotenv()
+
 async def get_db():
     mongo_uri = os.getenv('MONGO_URI')
     client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri)
