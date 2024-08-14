177bc33175f739a3063d9d5651c4c096a988b016
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-08-14 16:49:53 -0400

    show mongo uri

--- database/mongo.py
+++ database/mongo.py
@@ -6,5 +6,6 @@ load_dotenv()
 
 async def get_db():
     mongo_uri = os.getenv('MONGO_URI')
+    print(mongo_uri)
     client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri)
     return client.otakusenpai
