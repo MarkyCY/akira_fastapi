e8ea6d13e96cc9de456ea67984345dc9ca6e4460
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-10-12 19:26:04 -0400

    Add photo and items to example

--- routes/users.py
+++ routes/users.py
@@ -1,9 +1,11 @@
-from fastapi import Depends, APIRouter, Security
+from fastapi import Depends, APIRouter, Security, HTTPException
+from fastapi.responses import FileResponse
 from typing import Annotated
 
 from models.users import User
 from funcs.users import get_current_active_user
-from funcs.settings import get_password_hash
+
+import os
 
 Users = APIRouter()
 
@@ -14,15 +16,24 @@ async def read_users_me(
 ):
     return current_user  # Retorna la información del usuario actual
 
-# Endpoint para obtener los items del usuario actual
-@Users.get("/users/me/items/")
-async def read_own_items(
-    current_user: Annotated[User, Security(get_current_active_user, scopes=["user"])],
-):
-    return [{"item_id": "Foo", "owner": current_user.username}]  # Retorna una lista de items pertenecientes al usuario
-
 @Users.get("/users/admin/")
 async def read_admin_data(
     current_user: Annotated[User, Security(get_current_active_user, scopes=["admin"])],
 ):
     return {"admin_data": "This is secret data only for admins!"}
+
+
+@Users.get("/user/photo/{user_id}")
+async def get_user_photo(user_id: int):
+    # Construir el nombre de archivo esperado basado en el user_id
+    image_filename = f"{user_id}.jpg"
+    image_full_path = os.path.join("users/photo", image_filename)
+
+    # Verificar si el archivo existe
+    if not os.path.exists(image_full_path):
+        raise HTTPException(status_code=404, detail="Image not found")
+    
+    # Devolver la imagen como respuesta con encabezados de cache
+    response = FileResponse(image_full_path, media_type="image/jpeg")
+    response.headers["Cache-Control"] = "public, max-age=31536000, immutable"
+    return response
