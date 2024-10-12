e68a4ff4c60ff58c1cf317ebae9ed3c1fde75ad6
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-10-12 19:26:53 -0400

    Add model

---
+++ models/group_stats.py
@@ -0,0 +1,45 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+class Members(BaseModel):
+    current: int
+    previous: int
+
+class Viewers(BaseModel):
+    current: int
+    previous: int
+
+class Messages(BaseModel):
+    current: int
+    previous: int
+
+class Posters(BaseModel):
+    current: int
+    previous: int
+
+class Period(BaseModel):
+    min_date: int  # Unix timestamp (en segundos)
+    max_date: int  # Unix timestamp (en segundos)
+
+class TopAdmin(BaseModel):
+    user_id: int
+    first_name: str
+    deleted: int
+    kicked: int
+    banned: int
+
+class TopUser(BaseModel):
+    user_id: int
+    first_name: str
+    messages: int
+    avg_chars: int
+
+class StatsDaily(BaseModel):
+    _id: str  # MongoDB uses _id as the primary key
+    members: Members
+    viewers: Viewers
+    messages: Messages
+    period: Period
+    posters: Posters
+    top_admins: List[TopAdmin]
+    top_users: List[TopUser]
