b38b61e8468952aad2a376dd321a23c250b56d16
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-10-13 15:41:58 -0400

    Get photo change

--- routes/users.py
+++ routes/users.py
@@ -1,11 +1,13 @@
-from fastapi import Depends, APIRouter, Security, HTTPException
-from fastapi.responses import FileResponse
+from fastapi import Depends, APIRouter, Security, HTTPException, Response
 from typing import Annotated
 
 from models.users import User
 from funcs.users import get_current_active_user
 
-import os
+from database.mongo import get_db
+
+import requests
+from io import BytesIO
 
 Users = APIRouter()
 
@@ -23,17 +25,39 @@ async def read_admin_data(
     return {"admin_data": "This is secret data only for admins!"}
 
 
+# @Users.get("/user/photo/{user_id}")
+# async def get_user_photo(user_id: int):
+#     # Construir el nombre de archivo esperado basado en el user_id
+#     image_filename = f"{user_id}.jpg"
+#     image_full_path = os.path.join("users/photo", image_filename)
+
+#     # Verificar si el archivo existe
+#     if not os.path.exists(image_full_path):
+#         raise HTTPException(status_code=404, detail="Image not found")
+    
+#     # Devolver la imagen como respuesta con encabezados de cache
+#     response = FileResponse(image_full_path, media_type="image/jpeg")
+#     response.headers["Cache-Control"] = "public, max-age=31536000, immutable"
+#     return response
+
+
 @Users.get("/user/photo/{user_id}")
 async def get_user_photo(user_id: int):
-    # Construir el nombre de archivo esperado basado en el user_id
-    image_filename = f"{user_id}.jpg"
-    image_full_path = os.path.join("users/photo", image_filename)
+    db = await get_db()
+    users = db.users
+    user = await users.find_one({"user_id": user_id})
 
-    # Verificar si el archivo existe
-    if not os.path.exists(image_full_path):
+    if not user:
         raise HTTPException(status_code=404, detail="Image not found")
     
-    # Devolver la imagen como respuesta con encabezados de cache
-    response = FileResponse(image_full_path, media_type="image/jpeg")
-    response.headers["Cache-Control"] = "public, max-age=31536000, immutable"
-    return response
+    avatar = user.get("avatar")
+
+    if not avatar:
+        raise HTTPException(status_code=404, detail="Image not found")
+    print(avatar)
+    response = requests.get(avatar)
+    if response.status_code != 200:
+        raise HTTPException(status_code=404, detail="Image not found")
+
+    image_data = BytesIO(response.content)
+    return Response(content=image_data.getvalue(), media_type="image/jpeg", headers={"Cache-Control": "public, max-age=31536000, immutable"})
