3dc67c739d2de979ceb4dac69efd8149240866d2
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-10-29 03:12:37 -0400

    Fix model

--- models/contest.py
+++ models/contest.py
@@ -1,22 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from bson import ObjectId
+from typing import List, Optional
+
+class UserSubscription(BaseModel):
+    user: str = Field(..., alias="$numberLong")
 
 class ContestModel(BaseModel):
     _id: str
     type: str
-    amount_photo: int | None = None
-    amount_video: int | None = None
-    amount_text: int | None = None
+    amount_photo: Optional[int] = None
+    amount_video: Optional[int] = None
+    amount_text: Optional[int] = None
     title: str
     description: str
-    img: str | None = "https://i.pinimg.com/originals/50/e6/79/50e679b4d2a6195d10deaa80d738d3b3.jpg"
+    img: str = "https://i.pinimg.com/originals/50/e6/79/50e679b4d2a6195d10deaa80d738d3b3.jpg"
     status: str
-    start_date: int | None = None
+    start_date: Optional[int] = None
     end_date: int
-    subscription: list[int] | None = None
+    subscription: Optional[List[UserSubscription]] = None
     created_by: int
+    disqualified: Optional[List[UserSubscription]] = None
 
     class Config:
         orm_mode = True
         json_encoders = {ObjectId: str}
-  
+        allow_population_by_field_name = True
