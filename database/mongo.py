2081094ade1d7ddf81d7e62972ee5b4f71c9c03c
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-11-12 01:00:11 -0500

    Fix Mongo slow

--- database/mongo.py
+++ database/mongo.py
@@ -2,14 +2,15 @@ import os
 import motor.motor_asyncio
 
 # Resolve DNS for Termux [ Disable if you dont use Termux ]
-#import dns.resolver
-#dns.resolver.default_resolver=dns.resolver.Resolver(configure=False)
-#dns.resolver.default_resolver.nameservers=['8.8.8.8']
+# import dns.resolver
+# dns.resolver.default_resolver=dns.resolver.Resolver(configure=False)
+# dns.resolver.default_resolver.nameservers=['8.8.8.8']
 
 from dotenv import load_dotenv
 load_dotenv()
 
+mongo_uri = os.getenv('MONGO_URI')
+client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri)
+
 async def get_db():
-    mongo_uri = os.getenv('MONGO_URI')
-    client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri)
     return client.otakusenpai
