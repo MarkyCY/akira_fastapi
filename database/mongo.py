c7effe8339c6624ddc306e6a0a3fa3508433fcaf
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2025-05-08 16:01:32 -0400

    Fast Mongo connetion

--- database/mongo.py
+++ database/mongo.py
@@ -9,8 +9,8 @@ import motor.motor_asyncio
 from dotenv import load_dotenv
 load_dotenv()
 
+mongo_uri = os.getenv('MONGO_URI')
+client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri)
+
 async def get_db():
-    mongo_uri = os.getenv('MONGO_URI')
-    print(mongo_uri)
-    client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri)
     return client.otakusenpai
